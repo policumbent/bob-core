934aa8a3e69b3091d4ced9fa6f261f33fc446c93
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2022-07-18 23:12:18 +0200

    add: sensor methods for mqtt and `Message` class

--- core/mqtt.py
+++ core/mqtt.py
@@ -32,9 +32,98 @@ class Mqtt:
     def _get_client(self):
         return self._client
 
-    async def publish(self, topic, message):
-        return await self._client.publish(topic, payload=message.encode())
+    async def publish(self, topic: str, message: str):
+        """Publish a messege into a MQTT topic
 
-    async def subscribe(self, topic):
-        await self._client.subscribe(f"{topic}/#")
-        return self._client.filtered_messages(f"{topic}/+")
+        :param topic: MQTT topic
+        :param message: message to write
+        """
+
+        await self._client.publish(topic, payload=message.encode())
+
+    async def subscribe(self, topic: str or list):
+        """Read messeges of a topic from an async iterator
+
+        :param topic: MQTT topic, can be a list of topic
+        """
+
+        if isinstance(topic, str):
+            await self._client.subscribe(f"{topic}/#")
+            return self._client.unfiltered_messages()
+        else:
+            for t in topic:
+                await self._client.subscribe(f"{t}/#")
+            return self._client.unfiltered_messages()
+
+    async def sensor_publish(
+        self,
+        sensor: str,
+        data: str or int or float = None,
+    ):
+        """Publish a messege into a sensor topic
+
+        :param data: data to write to the topic
+        :param sensor: specific sensor of the module, can be of the format <module>/<sensor>
+        """
+
+        if isinstance(data, int) or isinstance(data, float):
+            data = str(data)
+
+        await self.publish(f"sensors/{sensor}", data)
+
+    async def sensor_subscribe(self, sensor: str or list = None):
+        """Read messeges of a sensor topic
+
+        :param sensor: specific sensor of the module, can be of the format <module>/<sensor>
+        """
+
+        if not sensor:
+            # subscribe to all sensors
+
+            return await self.subscribe("sensors")
+        elif isinstance(sensor, list):
+            # substcibe to a subset of sensors
+
+            sensors = [f"sensors/{s}" for s in sensor]
+            return await self.subscribe(sensors)
+        else:
+            # subscrive to only one sensor
+
+            return await self.subscribe(f"sensors/{sensor}")
+
+
+class Message:
+    """Decoder class for client messages"""
+
+    def __init__(self, msg):
+        self._raw_message = msg
+        self._topic = msg.topic
+        self._value = msg.payload.decode()
+
+    @property
+    def sensor(self):
+        return "/".join(self._topic.split("/")[1:])
+
+    @property
+    def module(self):
+        fields = self._topic.split("/")
+
+        if len(fields) > 2:
+            return fields[1]
+
+        return None
+
+    @property
+    def value(self):
+        # transform into int
+        if self._value.isdigit():
+            return int(self._value)
+
+        # transform into float
+        try:
+            return float(self._value)
+        except:
+            pass
+
+        # return the string
+        return self._value
--- tests/mqtt_test.py
+++ tests/mqtt_test.py
@@ -3,6 +3,7 @@ import os
 import pytest
 
 from core import Mqtt
+from core.mqtt import Message
 
 broker = "localhost"
 run_local = not os.getenv("CI")
@@ -14,8 +15,6 @@ pytest_plugins = "pytest_asyncio"
     run_local, reason="This test can be run only with an active mqtt broker"
 )
 async def test_mqtt_publish():
-    # publish message on topic
-
     async with Mqtt("localhost") as client:
         for i in range(4):
             await client.publish(f"test_topic/ciao{i}", f"test{i}")
@@ -38,9 +37,74 @@ async def test_mqtt_subscribe():
             message_loop = await client.subscribe(f"test_topic")
             async with message_loop as messages:
                 async for msg in messages:
-                    assert msg.payload.decode() == f"test{count}"
+                    msg = Message(msg)
+
+                    assert msg.value == f"test{count}"
                     count += 1
 
+                    if count >= num_msg:
+                        break
+
+    messages = 20
+    await asyncio.gather(subscriber(messages), publisher(messages))
+
+
+@pytest.mark.asyncio
+@pytest.mark.skipif(
+    run_local, reason="This test can be run only with an active mqtt broker"
+)
+async def test_mqtt_sensor_publish():
+    async with Mqtt("localhost") as client:
+        await client.sensor_publish(f"ant", 12)
+        await client.sensor_publish(f"ant", 12.3)
+        await client.sensor_publish(f"ant", "12.4")
+
+
+@pytest.mark.asyncio
+@pytest.mark.skipif(
+    run_local, reason="This test can be run only with an active mqtt broker"
+)
+async def test_mqtt_sensor_subscribe():
+    sensors = ["acc", "ant/hall", "bme", "altro"]
+
+    async def publisher(num_msg):
+        await asyncio.sleep(1)
+
+        async with Mqtt("localhost") as client:
+
+            for _ in range(num_msg // 4):
+                await client.sensor_publish(sensors[0], 12)
+                await client.sensor_publish(sensors[1], 12.11)
+                await client.sensor_publish(sensors[2], 13.11)
+                await client.sensor_publish(sensors[3], "hola")
+
+    async def subscriber(num_msg, count=0):
+        async with Mqtt("localhost") as client:
+            message_loop = await client.sensor_subscribe(sensors)
+            async with message_loop as messages:
+                async for msg in messages:
+                    msg = Message(msg)
+
+                    if msg.sensor == sensors[0]:
+                        assert msg.value == 12
+                        assert msg.module == None
+
+                    elif msg.sensor == sensors[1]:
+                        assert msg.value == 12.11
+                        assert msg.module == "ant"
+
+                    elif msg.sensor == sensors[2]:
+                        assert msg.value == 13.11
+                        assert msg.module == None
+
+                    elif msg.sensor == sensors[3]:
+                        assert msg.value == "hola"
+                        assert msg.module == None
+
+                    else:
+                        assert False
+
+                    count += 1
                     if count >= num_msg:
                         break
 
